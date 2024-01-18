1f17c326e87e7d4ce102e7a2e8db6442e4156b8b
Yojith Kaustabh <yojith212@gmail.com>
2024-01-19 00:01:44 +0530

    day0 commit

---
+++ lib/auth.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget{
+  const AuthScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    
+  }
+}
+
+class _AuthScreenState extends 
